1a8e40fc95d492e32d0fdb89f05ba82d5bacff8c
Luan Vieira <luanvieira.er@gmail.com>
2020-08-14 14:37:08 -0300

    Corrigido mapeamento da rota HOME
    Por padrão o express bloqueia qualquer requisição que não esteja mapeada, o código anterior não mapeava a rota da home que é acessada apenas com uma barra (/)
    O próximo passo será verificar as mensagens e testar as requisições com o Postman.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express'),
 app = express(),
+
 port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
 Message = require('./api/models/msgModel'),
 bodyParser = require('body-parser');
+
+app.get("/", function(req, res) {
+    console.log("Inicializado Server RESTful | Porta: "+ port);
+    res.send("Inicializado Server RESTful | Porta: "+ port);
+})
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/msgdb');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,4 +18,3 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/msgRoutes');
 routes(app);
 app.listen(port);
-console.log('Message RESTful API server started on: ' + port);
