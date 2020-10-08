ee034b7ef3b250518c4d8c0ecb8632b4e44a26be
Luan Vieira <luanvieira.er@gmail.com>
2020-10-08 16:49:45 -0300

    Adicionada Integração de Monte Carlo

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 
-port = process.env.PORT || 3000,
-mongoose = require('mongoose'),
+port = process.env.PORT || 3000;
+/*mongoose = require('mongoose'),
 Message = require('./api/models/msgModel'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser');*/
 
 app.get("/", function(req, res) {
-    console.log("Inicializado Server RESTful | Porta: "+ port);
-    res.send("Inicializado Server RESTful | Porta: "+ port);
+    console.log("Inicializado Server RESTful | Porta: "+ port + " Pi Estimado: " + getPi()) ;
+    res.send("Pi Estimado: " + getPi());
 })
 
 var uristring =
@@ -17,7 +17,22 @@ var uristring =
     process.env.MONGOHQ_URL ||
     'mongodb://localhost/msgdb';
 
-mongoose.Promise = global.Promise;
+app.listen(port);
+
+function getPi(){
+    var n = 10000000, inside = 0, x, y, z;
+    for (i = 0; i<n; i++){
+        x = Math.random();
+        y = Math.random();
+        if (Math.sqrt(x * x + y * y) <= 1){
+            inside++;
+        }
+    }
+    return 4 * inside / n;
+}
+
+//MONGOOSE DEPRECATED
+/*mongoose.Promise = global.Promise;
 mongoose.connect(uristring, function (err, res) {
     if (err) {
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
@@ -28,5 +43,4 @@ mongoose.connect(uristring, function (err, res) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var routes = require('./api/routes/msgRoutes');
-routes(app);
-app.listen(port);
+routes(app);*/
